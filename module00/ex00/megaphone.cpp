d5193619240dc64ee547ec66321e989bcea7bef5
Houssame Ait Hsain <houssame1337@gmail.com>
2023-10-16 15:50:23 +0100

    100%

---
+++ module00/ex00/megaphone.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cstdlib>
+#include <cctype>
+
+int main(int ac, char **av)
+{
+    std::string str;
+    int i;
+
+    if (ac == 1)
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+    for (int j = 1; j < ac; j++)
+    {
+        i = 0;
+        str = av[j];
+        for (; str[i]; i++)
+        {
+            str[i] = std::toupper(str[i]);
+        }
+        std::cout << str;
+    }
+    std::cout << std::endl;
+    return (EXIT_SUCCESS);
+}
